5e7d3ef47bb6c0c61cbbf2d772d6fd75cfc47889
Jimmy Lin <jmyjlin@gmail.com>
2026-05-19 22:35:15 -0700

    Initial Commit

---
+++ GroceryBuddy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GroceryBuddy
+//
+//  Created by Jimmy Lin on 5/19/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GroceryBuddy/GroceryBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryBuddyApp.swift
+//  GroceryBuddy
+//
+//  Created by Jimmy Lin on 5/19/26.
+//
+
+import SwiftUI
+
+@main
+struct GroceryBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
